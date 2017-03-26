113b122632495e0995f6c46d12d28421a2210a23
Esther Jun Kim <esther.jun.kim@gmail.com>
2017-03-25 21:41:11 -0700

    Fix the margins using d3 margin conventions

--- js/directives.js
+++ js/directives.js
@@ -37,20 +37,18 @@ directive("d3Bar", function($window) {
         // If we don"t pass any data, return out of the element
         if(!data) return;
 
-        var margin = {
-          top: 0,
-          bottom: 0,
-          left: 20,
-          right: 40
-        };
-        var padding = 20;
-        var width = d3.select(element[0]).node().offsetWidth - margin.left - margin.right;
-        var height = 200 - margin.top - margin.bottom;
+        var margin = {top: 0, right: 20, bottom: 0, left: 20},
+            padding = {top: 0, right: 0, bottom: 0, left: 60},
+            outerWidth = d3.select(element[0]).node().offsetWidth,
+            outerHeight = 200,
+            innerWidth = outerWidth - margin.left - margin.right,
+            innerHeight = outerHeight - margin.top - margin.bottom,
+            width = innerWidth - padding.left - padding.right,
+            height = innerHeight - padding.top - padding.bottom;
 
-        svg.attr("width", width + margin.left + margin.right)
-           .attr("height", height + margin.top + margin.bottom);
-
-        svg.append("g")
+        svg.attr("width", outerWidth)
+           .attr("height", outerHeight)
+          .append("g")
            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
         var x = d3.scale.linear()
@@ -71,27 +69,29 @@ directive("d3Bar", function($window) {
 
         var yAxis = d3.svg.axis()
                   .scale(y)
-                  .orient("left")
-                  .ticks(5);
+                  .orient("left");
 
-        svg.append("g")
-           .attr("class", "x axis")
-           .attr("transform", "translate(0," + (height - padding) + ")")
-           .call(xAxis);
+        var g = svg.append("g")
+                   .attr("transform", "translate(" + padding.left + "," + padding.top + ")");
 
-        svg.append("g")
-           .attr("class", "y axis")
-           .attr("transform", "translate(" + (margin.left + padding) + ",0)")
-           .call(yAxis);
+        g.append("g")
+         .attr("class", "x axis")
+         .attr("transform", "translate(0," + height + ")")
+         .call(xAxis);
 
-        var bars = svg.selectAll(".bar")
-                      .data(data)
-                      .enter()
-                      .append("g");
+        g.append("g")
+         .attr("class", "y axis")
+         .attr("transform", "translate(" + 0 + ",0)")
+         .call(yAxis);
+
+        var bars = g.selectAll(".bar")
+                    .data(data)
+                    .enter()
+                    .append("g");
 
         bars.append("rect")
             .attr("class", "bar")
-            .attr("x", margin.left + padding)
+            .attr("x", 0)
             .attr("y", function (d) {
               return y(d.label);
             })
@@ -103,7 +103,7 @@ directive("d3Bar", function($window) {
         bars.append("text")
             .attr("class", "label")
             .attr("x", function (d) {
-              return x(d.value) + margin.left + padding + 5;
+              return x(d.value) + 5;
             })
             .attr("y", function (d) {
               return y(d.label) + y.rangeBand() / 2 + 4;
